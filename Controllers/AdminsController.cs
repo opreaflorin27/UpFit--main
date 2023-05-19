cec0fe35a47f379779f0170405ac4cf07048904e
NTimea302 <80976659+NTimea302@users.noreply.github.com>
2023-05-19 16:26:29 +0300

    developing admin

--- Controllers/AdminsController.cs
+++ Controllers/AdminsController.cs
@@ -17,7 +17,37 @@ namespace UpFit__main.Controllers
         // GET: Admins
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ManageAdmins()
+        {
             return View(db.admins.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            using (CodeFirstDb db = new CodeFirstDb())
+            {
+                var adm = db.admins.SingleOrDefault(u => u.UserName == admin.UserName && u.Password == admin.Password);
+                if (adm != null)
+                {
+                    Session["UserID"] = adm.AdminID.ToString();
+                    Session["UserName"] = adm.UserName.ToString();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password.");
+                }
+            }
+            return View();
         }
 
         // GET: Admins/Create
@@ -64,5 +94,101 @@ namespace UpFit__main.Controllers
             }
             base.Dispose(disposing);
         }
+
+        //****************************CHAT-GPT****************************************88
+        // GET: Admins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdminID, UserName, Password")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Admin admin = db.admins.Find(id);
+            db.admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admins/ManageUsers
+        public ActionResult ManageUsers()
+        {
+            return View(db.users.ToList());
+        }
+
+        // GET: Admins/AddUser
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        // POST: Admins/AddUser
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("ManageUsers");
+            }
+
+            return View(user);
+        }
+
+
+        // GET: Admins/DeleteUser/5
+        public ActionResult DeleteUser(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admins/DeleteUser/5
+        [HttpPost, ActionName("DeleteUser")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteUserConfirmed(int id)
+        {
+            User user = db.users.Find(id);
+            db.users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("ManageUsers");
+        }
+
     }
 }
